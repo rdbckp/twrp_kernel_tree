90349bf70c1c3c1bff7f1fccc0f360ef66f9815c
rdbckp <ardibackup@gmail.com>
2026-08-10 00:40:07 +0700

    Update device.mk
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- device.mk
+++ device.mk
@@ -9,9 +9,9 @@ LOCAL_PATH := device/samsung/a02
 
 # --- Bagian Copy Files ---
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/root/system/etc/ueventd.rc:recovery/root/ueventd.rc \
-    $(LOCAL_PATH)/root/init.recovery.mt6739.rc:recovery/root/init.recovery.mt6739.rc \
-    $(LOCAL_PATH)/root/init.recovery.samsung.rc:recovery/root/init.recovery.samsung.rc \
-    $(LOCAL_PATH)/root/init.recovery.usb.rc:recovery/root/init.recovery.usb.rc \
-    $(LOCAL_PATH)/root/system/etc/recovery.fstab:recovery/root/system/etc/recovery.fstab \
-    $(LOCAL_PATH)/root/system/etc/twrp.flags:recovery/root/system/etc/twrp.flags
+    $(LOCAL_PATH)/recovery/root/system/etc/ueventd.rc:recovery/root/ueventd.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.mt6739.rc:recovery/root/init.recovery.mt6739.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.samsung.rc:recovery/root/init.recovery.samsung.rc \
+    $(LOCAL_PATH)/recovery/root/init.recovery.usb.rc:recovery/root/init.recovery.usb.rc \
+    $(LOCAL_PATH)/recovery/root/system/etc/recovery.fstab:recovery/root/system/etc/recovery.fstab \
+    $(LOCAL_PATH)/recovery/root/system/etc/twrp.flags:recovery/root/system/etc/twrp.flags
